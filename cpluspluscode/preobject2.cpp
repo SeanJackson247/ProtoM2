3b3f2343dc533e6e9b3d92d55cb4acdc51819ce9
SeanJackson247 <jacksonsean24@yahoo.co.uk>
2017-04-17 18:44:39 +0100

    Add files via upload

---
+++ cpluspluscode/preobject2.cpp
@@ -0,0 +1,6 @@
+#include "preobject2.h"
+
+PreObject2::PreObject2(LexNode* lexNode)
+{
+
+}
---
+++ cpluspluscode/preobject2.h
@@ -0,0 +1,10 @@
+#ifndef PREOBJECT2_H
+#define PREOBJECT2_H
+
+#include "lexnode.h"
+
+class PreObject2{
+public: PreObject2(LexNode *lexNode);
+};
+
+#endif // PREOBJECT2_H
